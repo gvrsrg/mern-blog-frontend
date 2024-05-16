3a397d01d3d0f9cc7f5e1e9e3969f68fd0211251
gvrsrg <gvrsrg@gmail.com>
2024-05-16 23:06:08 +0300

    baseUrl

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'http://localhost:4444',
-})
+//    baseURL: 'http://localhost:4444',
+    baseURL: 'https://mern-3qyu.onrender.com',})
 
 instance.interceptors.request.use(config => {
     config.headers.Authorization = window.localStorage.getItem('token') || ''
